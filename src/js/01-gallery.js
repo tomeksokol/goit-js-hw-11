86e4eeeb1cc94e700be1a56ab1e634802f6fdb40
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-04 15:44:10 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
@@ -10,39 +12,14 @@ console.log(galleryContainer);
 const photoContainer = galleryItems
   .map(
     galleryItem =>
-      `<div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a> </div>`,
+      `<a class="gallery__item" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-caption="${galleryItem.description}" alt="${galleryItem.description}"/></a>`,
   )
   .join('');
 galleryContainer.insertAdjacentHTML('beforeend', photoContainer);
 console.log(photoContainer);
 
-const keyClose = (e, instance) => {
-  if ('Escape' === e.key) {
-    instance.close();
-  }
-  console.log(e.key);
-};
-
-const images = document.querySelectorAll('.gallery__item .gallery__image');
-console.log(images);
-images.forEach(image => {
-  image.addEventListener('click', event => {
-    event.preventDefault();
-
-    basicLightbox
-      .create(
-        `<img width="1400" height="900" src="${image.dataset.source}" loading="lazy" alt="${image.alt}">`,
-        {
-          onClose: instance => {
-            galleryContainer.removeEventListener('keydown', e => keyClose(e, instance));
-            return true;
-          },
-          onShow: instance => {
-            galleryContainer.addEventListener('keydown', e => keyClose(e, instance));
-            return true;
-          },
-        },
-      )
-      .show();
-  });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
+console.log(lightbox);
