0bd04bfde931d9ee315af6a73ee32f21db79cbaa
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-06 12:13:13 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,5 @@
 const form = document.querySelector('.feedback-form');
-
-const message = '';
-const email = '';
-
-const emailInput = document.querySelector('.feedback-form input');
-
+const emailInput = document.querySelector('input');
 const textMessage = document.querySelector('textarea');
 
 updateOutput();
@@ -13,20 +8,16 @@ form.addEventListener('input', saveData);
 
 function saveData(evt) {
   evt.preventDefault();
-  const {
-    elements: { email, message },
-  } = evt.currentTarget;
-  localStorage.setItem('email', email.value);
+  localStorage.setItem('email', form.elements.email.value);
   console.log(form.elements.email.value);
-  localStorage.setItem('message', message.value);
+  localStorage.setItem('message', form.elements.message.value);
   console.log(form.elements.message.value);
   updateOutput();
-  // form.reset();
 }
 
 function updateOutput() {
-  emailInput.textContent = localStorage.getItem('email');
-  textMessage.textContent = localStorage.getItem('message');
+  emailInput.textContent = localStorage.getItem('email') || '';
+  textMessage.textContent = localStorage.getItem('message') || '';
 }
 
 form.addEventListener('submit', handleSubmit);
@@ -40,7 +31,5 @@ function handleSubmit(event) {
 
   console.log(`Email: ${email.value}, Message: ${message.value}`);
   event.currentTarget.reset();
+  textMessage.reset();
 }
-
-//const result = _.add(2, 3);
-//console.log(result);
