fa3eda88eeaf3b6016fedbd99f8370419d479149
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-12 13:28:20 +0200

    task2-resolved

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,8 +5,6 @@ import 'flatpickr/dist/flatpickr.min.css';
 require('flatpickr/dist/themes/material_blue.css');
 import Notiflix from 'notiflix';
 
-//data wybrana z kalendarzea przeformatowania na UTC i porównanie UTC do dzisiejszego UTC jak liczba większa button.disable = false
-
 const startDate = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
@@ -14,7 +12,11 @@ const daysOutput = document.querySelector('.timer span[data-days]');
 const hoursOutput = document.querySelector('.field span[data-hours]');
 const minutesOutput = document.querySelector('.field span[data-minutes]');
 const secondsOutput = document.querySelector('.field span[data-seconds');
-let utcTime = null;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 const options = {
   enableTime: true,
@@ -42,7 +44,10 @@ const options = {
 
           convertMs(ms);
           console.log(convertMs(ms));
-          daysOutput.textContent = `${convertMs(ms)}`;
+          daysOutput.textContent = String(Math.floor(ms / day));
+          hoursOutput.textContent = String(Math.floor((ms % day) / hour));
+          minutesOutput.textContent = String(Math.floor(((ms % day) % hour) / minute));
+          secondsOutput.textContent = String(Math.floor((((ms % day) % hour) % minute) / second));
         }, 1000);
       };
 
@@ -71,7 +76,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
