961ffde92fd8f80575329be914c50475165c1cd1
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-15 15:51:10 +0200

    0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
+//import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const searchingBox = document.querySelector('#search-box');
+const background = document.querySelector('body');
+
+//country searching styles
+
+// Add a box shadow on focus
+searchingBox.addEventListener('focus', e => {
+  e.target.style.outline = 'none';
+  e.target.style.borderColor = 'red';
+});
+
+// Remove the box shadow when the user doesn't focus anymore
+searchingBox.addEventListener('blur', e => {
+  e.target.style.borderColor = '#212121';
+  e.target.style.outline = 'none';
+});
