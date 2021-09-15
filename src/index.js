a936e44927c8e294eba05d2bb7f82209336c5786
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-15 19:08:29 +0200

    0

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const DEBOUNCE_DELAY = 300;
 const searchingBox = document.querySelector('#search-box');
 const background = document.querySelector('body');
 
-//country searching styles
-//document.body.style.backgroundImage = "url('./images/background_img.jpg')";
+//STYLES
+
+//background image style
+document.body.style.backgroundImage = 'radial-gradient(#a1a0a0 1%, transparent 10%)';
+document.body.style.backgroundSize = '70px 70px';
 // Add a box shadow on focus
 searchingBox.addEventListener('focus', e => {
   e.target.style.outline = 'none';
-  e.target.style.borderColor = 'red';
+  e.target.style.border = '2px solid #3b6deb';
 });
-
 // Remove the box shadow when the user doesn't focus anymore
 searchingBox.addEventListener('blur', e => {
   e.target.style.borderColor = '#212121';
-  e.target.style.outline = 'none';
+  searchingBox.style.border = '1px solid #212121';
 });
+searchingBox.style.marginLeft = '20px';
+searchingBox.style.marginTop = '20px';
+searchingBox.style.maxWidth = 'none';
+searchingBox.style.width = '650px';
+searchingBox.style.height = '70px';
+//searchingBox.style.border = '2px solid #212121';
