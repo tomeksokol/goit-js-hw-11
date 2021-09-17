c85290f027453abcc9170116f4afeb626e1c5606
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-17 13:55:44 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
 //import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const searchingBox = document.querySelector('#search-box');
+const searchingBox = document.querySelector('input#search-box');
 const background = document.querySelector('body');
+const countryLIst = document.querySelector('.country-list');
+const countryInfo = document.querySelector('country-info');
 
+const eventHandler = event => {
+  let name = event.currentTarget.value;
+  fetchCountries(name)
+    .then(name => renderCountryList(name))
+    .catch(error => console.log(error));
+};
+
+searchingBox.addEventListener('input', eventHandler);
+
+function renderCountryList(name) {
+  const markup = name
+    .map(country => {
+      return `<li>
+          <img class="flag__img" src="${country.flag}" alt="Flag of ${country.name}" width="50" height="30"><span class="country__name">${country.name}<span></img>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>languages</b>: ${country.languages[0].name}</p>
+        </li>`;
+    })
+    .join('');
+  countryLIst.innerHTML = markup;
+}
 // 1) Create function fetching data Countries
 // 1a) Create const url
 // 1b) Create URLSearchParams
@@ -32,4 +57,3 @@ searchingBox.style.marginTop = '20px';
 searchingBox.style.maxWidth = 'none';
 searchingBox.style.width = '650px';
 searchingBox.style.height = '70px';
-//searchingBox.style.border = '2px solid #212121';
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,9 @@
 function fetchCountries(name) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then((response) => {
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
   });
 }
+export { fetchCountries };
