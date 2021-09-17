5b12858dc8d4d6107a8246fdf02ee7bfaa8273d8
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-17 10:46:20 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,8 @@
-function fetchCountries(name) {}
+function fetchCountries(name) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
