e910d251b163e9a5ebf7dbc2ecee5898cb156ac9
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-20 08:31:56 +0200

    3

--- src/index.js
+++ src/index.js
@@ -14,17 +14,31 @@ const countryInfo = document.querySelector('.country-info');
 const eventHandler = event => {
   let name = event.currentTarget.value;
   console.log(fetchCountries(name));
-  fetchCountries(name)
-    .then(name => {
-      if (name.length > 10) {
-        console.log('too many');
-      } else if (name.length > 2 && name.length < 10) {
-        renderCountryList(name);
-      } else if (name.length === 1) {
-        renderCountryInfo(name);
-      }
-    })
-    .catch(error => console.log(error));
+  if (name === '') {
+    Notiflix.Notify.success('Please enter a country name.');
+    countryLIst.style.display = 'none';
+    countryInfo.style.display = 'none';
+  } else {
+    fetchCountries(name)
+      .then(name => {
+        if (name.length > 10) {
+          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (name.length >= 2 && name.length <= 10) {
+          renderCountryList(name);
+          countryLIst.style.display = 'block';
+          countryInfo.style.display = 'none';
+        } else if (name.length === 1) {
+          renderCountryInfo(name);
+          countryLIst.style.display = 'none';
+          countryInfo.style.display = 'block';
+        }
+      })
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name.');
+        countryLIst.style.display = 'none';
+        countryInfo.style.display = 'none';
+      });
+  }
 };
 
 // searchingBox.addEventListener('input', debounce(eventHandler, 300));
@@ -33,7 +47,7 @@ searchingBox.addEventListener('input', eventHandler);
 function renderCountryList(name) {
   const markup = name
     .map(country => {
-      return `<li>
+      return `<li class="country__list">
           <img class="flag__img" src="${country.flag}" alt="Flag of ${country.name}" width="50" height="30"><span class="country__name">${country.name}<span></img>
         </li>`;
     })
@@ -44,7 +58,7 @@ function renderCountryList(name) {
 function renderCountryInfo(name) {
   const markup = name
     .map(country => {
-      return `<li>
+      return `<li class="country__info">
           <img class="flag__img" src="${country.flag}" alt="Flag of ${country.name}" width="50" height="30"><span class="country__name">${country.name}<span></img>
           <p><b>Capital</b>: ${country.capital}</p>
           <p><b>Population</b>: ${country.population}</p>
@@ -54,6 +68,10 @@ function renderCountryInfo(name) {
     .join('');
   countryInfo.innerHTML = markup;
 }
+
+// function countryListClear() {
+//   return '<li></li>';
+// }
 // 1) Create function fetching data Countries
 // 1a) Create const url
 // 1b) Create URLSearchParams
@@ -63,9 +81,11 @@ function renderCountryInfo(name) {
 
 //STYLES
 
-//background image style
+//background body style
 document.body.style.backgroundImage = 'radial-gradient(#a1a0a0 1%, transparent 10%)';
 document.body.style.backgroundSize = '70px 70px';
+countryLIst.style.listStyle = 'none';
+countryInfo.style.listStyle = 'none';
 // Add a box shadow on focus
 searchingBox.addEventListener('focus', e => {
   e.target.style.outline = 'none';
