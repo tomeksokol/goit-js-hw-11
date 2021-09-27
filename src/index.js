22774512d682e53a7e7291fff5a256e47f5379f3
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-09-27 21:24:41 +0200

    3
    fetching with input and button fixed

--- src/index.js
+++ src/index.js
@@ -6,51 +6,76 @@ import Notiflix from 'notiflix';
 
 const searchgBox = document.querySelector('input');
 const searchBtn = document.querySelector('.search-btn');
+const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 
-// async function fetchImages(name) {
-//   const response = await fetch(
-//     `https://pixabay.com/api/?key=23580980-4f75151f85975025bb6074227&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`,
-//   );
-//   const photos = await response.json();
-//   return console.log(photos);
-// }
-
-// const searchingResults = async event => {
-//   let name = event.target.value;
-//   fetchImages()
-//   .then(photos => console.log(photos))
-//   .catch(error => console.log(error));
-//   console.log(photos);
-//   console.log(fetchImages(name));
-//   try {
-//     const photos = await fetchImages();
-//     console.log(photos);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-function fetchPhotos(name) {
-  return fetch(
-    `https://pixabay.com/api/?key=23580980-4f75151f85975025bb6074227&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`,
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+async function fetchImages(name) {
+  const response = await fetch(
+    `https://pixabay.com/api/?key=23580980-4f75151f85975025bb6074227&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`,
+  );
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return await response.json();
 }
 
-function searchingResults(event) {
-  let name = event.target.value;
-  console.log(fetchPhotos(name));
-  fetchPhotos(name)
-    .then(name => console.log(name))
+// function fetchPhotos(name) {
+//   return fetch(
+//     `https://pixabay.com/api/?key=23580980-4f75151f85975025bb6074227&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`,
+//   ).then(data  => data.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+// }
+
+// function searchingResults(event) {
+//   let name = event.target.value;
+//   console.log(fetchPhotos(name));
+//   fetchPhotos(name)
+//     .then(name => console.log(name))
+//     .catch(error => console.log(error));
+// }
+
+// let input = searchgBox.addEventListener('input', e => {
+//    let inputSearch = e.target.value;
+//    console.log(inputSearch);
+//  });
+
+// const searchingResults = async event => {
+//   console.log("Button is clicked");
+//   let name = inputSearch;
+//   fetchImages(name)
+//     .then(name => console.log(name))
+//     .catch(error => console.log(error));
+//   console.log(name);
+//   console.log(name.hits[0].pageURL);
+//   console.log(fetchImages(name));
+// };
+
+async function eventHandler(ev) {
+  ev.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = ev.currentTarget;
+  console.log(searchQuery.value);
+  let name = searchQuery.value;
+  console.log(name);
+  const fetching = await fetchImages(name)
+    .then(name => {
+      console.log(name);
+      console.log(name.hits.length);
+      if (name.hits.length > 0) {
+        Notiflix.Notify.success(`Hooray! We found ${name.totalHits} images.`);
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      }
+    })
     .catch(error => console.log(error));
 }
 
-searchBtn.addEventListener('click', searchingResults);
+searchForm.addEventListener('submit', eventHandler);
+//searchBtn.addEventListener('click', searchingResults);
 
 // function renderGallery(name) {
 //   const markup = name
